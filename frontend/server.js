69844e94e9c4b5215a64e6ca3724eae567f53f85
catherineyu8 <catherine.j.yu@gmail.com>
2024-09-17 15:15:58 -0400

    feat: actually adding the frontend files now

---
+++ frontend/server.js
@@ -0,0 +1,19 @@
+// // server.js
+// import express from "express";
+// import path from "path";
+
+// const app = express();
+// const port = 3000; // You can choose a different port if necessary
+
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // Basic route for testing
+// app.get("/", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// // Start the server
+// app.listen(port, () => {
+// 	console.log(`Server is running at http://localhost:${port}`);
+// });
